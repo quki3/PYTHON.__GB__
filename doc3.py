c563bf27b262a18404f911acdef687a70c13eb7f
quki <gabrielbrufau@gmail>
2022-01-03 16:55:08 -0300

    poo

---
+++ doc3.py
@@ -0,0 +1,44 @@
+
+# this is the first comment
+
+# numeros
+2+2 # 4
+
+50 - 5*6 # 20
+
+(50 - 5*6) / 4 # 5.0 division always return a floating point number
+
+17 // 3 # 5 floor division discards the fractional part
+
+17 % 3 # 2 the % operator return the remainder of the division
+
+2 ** 7 # 2 to the power of 7
+
+4 * 3.75 # there is a full soport for floating dot
+
+#variables
+width = 20 # the signal equal to use asignament value
+
+# string
+'single quotes'
+'doesn\'t' # use \' to escaoe the single quote
+"doesn't" # or use double quotes instead
+'"yes," they said."'
+'First line.\nSecond line. '
+r'C:\some\name' # r evita \n
+"""
+usage: [OPTION]
+    -h          Display this usage 
+    -H          Hostname to connect to
+"""
+# funciones 
+print('hello') # imprime en consola
+
+
+
+
+
+
+
+
+
